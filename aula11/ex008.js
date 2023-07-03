a91c4e2f7176892a62af797d752009461ad8d450
Marcos Milhomem <marcosapolo511@gmail.com>
2023-07-03 12:49:34 -0300

    ex008

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 30
+console.log(`A velocidade do eu carro é ${vel} Km/h`)
+if (vel > 80) {
+    console.log ('Você ultrapassou a velocidade permitida. <strong>MULTADO</strong>')
+}
+console.log('Dirija sempre com sinto de segurança para evitar acidentes!')
