96b012503c67be292b44f07af4456f0c10942961
Marcos Milhomem <marcosapolo511@gmail.com>
2023-08-12 01:33:19 -0300

    dio

--- dio/exercicio4.js
+++ dio/exercicio4.js
@@ -1,16 +1,16 @@
 const peso = 78;
-const altura = 1.2;
+const altura = 1.81;
 
-const imc = peso * (altura * altura);
+const imc = peso / Math.pow(altura,2);
 
 if (imc < 18.5) {
     console.log("Abaixo do Peso");
 } else if (imc >= 18.5 && imc < 25) {
     console.log ("Peso Normal");
 } else if (imc >= 25 && imc < 30) {
-    console.log ("Acima do eso");
+    console.log ("Acima do Peso");
 } else if (imc >= 30 && imc < 40) {
     console.log ("Obeso");
-} else if (imc > 40) {
+} else {
     console.log ("Obesidade Grave");
 }
