a3b3590cf01a21d37d4b6d0361f9d8744cc760a9
Marcos Milhomem <marcosapolo511@gmail.com>
2023-08-16 01:42:21 -0300

    Merge branch 'main' of https://github.com/MarcosMilhomem/JavaScript/dio

---
+++ CursoemVideo/aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 16;
+if (idade < 16) {
+    console.log("Não vota")
+} else if (idade >= 16 && idade < 18) {
+    console.log("Voto opcional")
+} else {
+    console.log ("Voto obrigatório")
+}
