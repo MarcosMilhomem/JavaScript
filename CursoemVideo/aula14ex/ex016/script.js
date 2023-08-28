eddb9ded80f7e7be00cee4754f334482c651f653
Marcos Milhomem <marcosapolo511@gmail.com>
2023-08-28 19:02:12 -0300

    curso em vídeo

---
+++ CursoemVideo/aula14ex/ex016/script.js
@@ -0,0 +1,10 @@
+function contando() {
+let inicio = document.querySelector('div#inicio')
+let fim = document.querySelector('div#fim')
+let passo = document.querySelector('div#passo')
+let res = inicio + passo
+while (res <= fim) {
+    console.log(`passo ${res}`)
+    inicio + passo
+}
+}
