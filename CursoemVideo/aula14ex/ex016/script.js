238935f5660a6a248431967ad3c9f773ec65d4c6
Marcos Milhomem <marcosapolo511@gmail.com>
2023-08-28 19:52:01 -0300

    curso em vídeo

--- CursoemVideo/aula14ex/ex016/script.js
+++ CursoemVideo/aula14ex/ex016/script.js
@@ -2,9 +2,6 @@ function contando() {
 let inicio = document.querySelector('div#inicio')
 let fim = document.querySelector('div#fim')
 let passo = document.querySelector('div#passo')
-let res = inicio + passo
-while (res <= fim) {
-    console.log(`passo ${res}`)
-    inicio + passo
-}
+let res = Number(inicio) + Number(passo)
+console.log(res)
 }
