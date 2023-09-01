4542d256ba2f5b5348d50b09347255eebbfb52be
Marcos Milhomem <marcosapolo511@gmail.com>
2023-09-01 01:19:58 -0300

    Curso em Vídeo

--- CursoemVideo/aula14ex/ex016/script.js
+++ CursoemVideo/aula14ex/ex016/script.js
@@ -1,11 +1,27 @@
 function contar() {
-let inicio = document.getElementById('inicio')
-let fim = document.getElementById('fim')
-let passo = document.getElementById('passo')
+    let inicio = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('passo')
 
-if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-    alert ('Está faltando preencher um campo')
-} else {
-    alert ('Tudo ok')
-} 
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('[ERRO] Os campos não estão preenchidos corretamente!')
+    } else {
+        res.innerHTML = `Contando: `
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (i < f) {
+            //contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            //contagem regressiva 
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3F4}`
+    }
 }
