fcfcd0ae56b2e2e37f03aca438a90cf6c038fff4
Marcos Milhomem <marcosapolo511@gmail.com>
2023-09-04 16:37:08 -0300

    Curso em vídeo

--- CursoemVideo/aula14ex/ex017/script.js
+++ CursoemVideo/aula14ex/ex017/script.js
@@ -1,1 +1,19 @@
-var horaAtualizado = new Date();
+function tabuada() {
+    let num = document.querySelector ('#num')
+    let tab = document.querySelector ('#seltab')
+
+    if (num.value.length == 0) {
+        alert ('Digite um número')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement ('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
