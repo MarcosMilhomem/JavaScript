30ca8c99e9efcb6b7a34083963a07b84e71ddf9d
Marcos Milhomem <marcosapolo511@gmail.com>
2023-09-08 01:12:17 -0300

    curso em vídeo

--- CursoemVideo/aula16ex/ex018/script.js
+++ CursoemVideo/aula16ex/ex018/script.js
@@ -1,1 +1,28 @@
-var horaAtualizado = new Date();
+let num = document.querySelector('#fnum')
+let lista = document.querySelector('#flista')
+let res = document.querySelector('res')
+let valores = []
+
+function add() {
+    if(isNumber(num.value) && inList(num.value, valores)) {
+        valores.push(Number(num.value))
+    }  else {
+        alert("Número inválido ou já existente")
+    }
+}
+
+function isNumber(n) {
+    if(Number(n) >= 1 && Number(n) <=100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inList(n, l) {
+    if(l.indexOf(Number(n) != -1)) {
+        return true
+    } else{
+        return false
+    }
+}
