f7fd8e696ef0b858db50a7c72bbb15e8972679bc
Marcos Milhomem <marcosapolo511@gmail.com>
2023-10-18 13:00:56 -0300

    estudos

--- CursoemVideo/aula16ex/ex018/script.js
+++ CursoemVideo/aula16ex/ex018/script.js
@@ -1,28 +1,60 @@
-let num = document.querySelector('#fnum')
-let lista = document.querySelector('#flista')
-let res = document.querySelector('res')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
 let valores = []
 
-function add() {
-    if(isNumber(num.value) && inList(num.value, valores)) {
-        valores.push(Number(num.value))
-    }  else {
-        alert("Número inválido ou já existente")
-    }
-}
-
-function isNumber(n) {
-    if(Number(n) >= 1 && Number(n) <=100) {
+function isNumero (n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
         return true
     } else {
         return false
     }
 }
 
-function inList(n, l) {
-    if(l.indexOf(Number(n) != -1)) {
+function inLista (n, l) {
+    if (l.indexOf(Number(n)) != -1) {
         return true
-    } else{
+    } else {
         return false
     }
-}   
+}
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) { 
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    } else (
+        window.alert('Valor inválida ou já encontrada na lista.')
+    )
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert ('Adicione algum valor')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if(valores[pos] < menor)
+            menor = valores[pos]
+        }
+        media = soma/tot
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todos, temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<p> menor valor adicionado foi ${maior}</p>`
+        res.innerHTML += `<p> menor valor adicionado foi ${menor}</p>`
+        res.innerHTML += `<p> Somando todos os valores , temos ${soma}</p>`
+        res.innerHTML += `<p> A média dos valores digitados é ${media}</p>`
+    }
+}
